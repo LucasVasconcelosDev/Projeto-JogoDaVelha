500ce67b042611215987e375116c4796487a58b0
Lucas Vasconcelos <lucas.jesus44@gmail.com>
2022-11-03 14:27:00 -0300

    criado component icone.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import Icon from "../icon/Icon"
 import styles from "./Header.module.css"
 import Title from "../title/Title"
 import Subtitle from "../subtitle/Subtitle"
@@ -5,8 +6,11 @@ import Subtitle from "../subtitle/Subtitle"
 function Header(){
     return(
         <div className={styles.header}>
-        <Title>Jogo Da Velha</Title>
-        <Subtitle>Criado por Lucas Vasconcelos</Subtitle>
+          <Title>Jogo Da Velha</Title>
+          <Subtitle>Criado por Lucas Vasconcelos</Subtitle>
+          <div className={styles.iconContent}>
+            <Icon iconName="github" link="https://github.com/LucasVasconcelosDev"/>
+          </div>
       </div>
     )
 }
