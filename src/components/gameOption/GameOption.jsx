ed0552db95daee5962a1a13feb22b1640f74e5a4
Lucas Vasconcelos <lucas.jesus44@gmail.com>
2022-11-03 16:25:28 -0300

    criado estados no GameOption

--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,9 +1,15 @@
 import styles from "./GameOption.module.css"
+import Icon from "../icon/Icon"
 
-function GameOption(){
+function GameOption({status}){
     return(
         <div className={styles.gameOption}>
-            
+            {
+                status === 1 && <Icon iconName="circle"/>
+            }
+            {
+                status === -1 && <Icon iconName="x"/>
+            }
         </div>
     )
 }
